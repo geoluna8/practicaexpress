65d15b5a0657eacea675971ca759249aaf7500c7
MacPro <macpro@MacBook-Pro-de-MacPro.local>
2021-10-08 12:49:11 -0500

    se agrega cors y refactor de rutas

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // importando dependencias
 const Express = require("express");
 const { connect } = require("mongoose");
+const cors = require('cors');
 // llamando al
 const { agenda } = require("./schema.js");
 const FakeData = require("./fakeData.json");
@@ -15,6 +16,8 @@ Server.use(Express.static("public"));
 Server.use(require('body-parser').urlencoded({extended: false}));
 Server.use(require('body-parser').json());
 
+Server.use(cors({origin: '*'}));
+
 // Parametros de conexion
 const USER = "root";
 const PASSWORD = "geovanny8";
@@ -27,32 +30,23 @@ const OPTIONS = {
     useUnifiedTopology: true
 };
 
-// Router para crear datos de manera aleatoria
-Server.use("/random", (request, response) => {
-    // Se consiguen los nodos del archivo FakeData
-    const { names, lastNames } = FakeData;
-    // Consiguiendo un index de manera aleatoria
-    const NAME = Math.floor(Math.random() * (names.length - 0));
-    const LAST_NAME = Math.floor(Math.random() * (lastNames.length - 0));
-    // Preparando los datos que seran enviados a mongodb
-    const DATA = {
-        name: names[NAME],
-        lastName: lastNames[LAST_NAME],
-        age: NAME * 2,
-        random: NAME * LAST_NAME
-    };
+//Ruta por default
+Server.get('/api', (req, res) => {
+    res.send("Welcome to Geovanny's Express Server");
+});
 
-    // Se indica que se crea un nuevo registro
-    const AGEMDA = new agenda(DATA);
-
-    // Se recibe la respuesta generada al crear un nuevo registro.
-    AGEMDA.save((error, data) => {
+// Router para consultar todos los datos generados.
+Server.get("/api/consulta", (request, response) => {
+ 
+    // Generamos una busqueda completa.
+    console.log("consulta");
+    agenda.find({}, (error, data) => {
         // En caso de error mostramos el problema
         if (error) {
             response.status(404);
             response.json(error);
         } else {
-            // en caso de que todo salga correcto enviamos la respuesta.
+             // en caso de que todo salga correcto enviamos la respuesta.
             response.status(200);
             response.json(data);
         }
@@ -60,8 +54,6 @@ Server.use("/random", (request, response) => {
 });
 
 Server.post("/api/alta", (request, response) => {
-    response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");  
 
     console.log("alta", request.body);
 
@@ -91,19 +83,33 @@ Server.post("/api/alta", (request, response) => {
 
     }); 
 
-// Routere para consultar todos los datos generados.
-Server.use("/api", (request, response) => {
-    response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");  
-    // Generamos una busqueda completa.
-    console.log("consulta");
-    agenda.find({}, (error, data) => {
+
+// Router para crear datos de manera aleatoria
+Server.use("/random", (request, response) => {
+    // Se consiguen los nodos del archivo FakeData
+    const { names, lastNames } = FakeData;
+    // Consiguiendo un index de manera aleatoria
+    const NAME = Math.floor(Math.random() * (names.length - 0));
+    const LAST_NAME = Math.floor(Math.random() * (lastNames.length - 0));
+    // Preparando los datos que seran enviados a mongodb
+    const DATA = {
+        name: names[NAME],
+        lastName: lastNames[LAST_NAME],
+        age: NAME * 2,
+        random: NAME * LAST_NAME
+    };
+
+    // Se indica que se crea un nuevo registro
+    const AGEMDA = new agenda(DATA);
+
+    // Se recibe la respuesta generada al crear un nuevo registro.
+    AGEMDA.save((error, data) => {
         // En caso de error mostramos el problema
         if (error) {
             response.status(404);
             response.json(error);
         } else {
-             // en caso de que todo salga correcto enviamos la respuesta.
+            // en caso de que todo salga correcto enviamos la respuesta.
             response.status(200);
             response.json(data);
         }
